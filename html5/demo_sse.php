0395c947bee363256c47c4eec421fbf062cde2ab
xxg3053 <xxg3053@qq.com>
2015-07-20 16:17:26 +0800

    sse后台

--- html5/demo_sse.php
+++ html5/demo_sse.php
@@ -3,6 +3,6 @@ header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
 $time = date('r');
-echo "data: The server time is: {$time}\n\n";
+echo "data: :The server time is {$time}\n\n";
 flush();
 ?>
